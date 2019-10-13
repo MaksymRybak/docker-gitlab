c924aa9ef45a2e7e8c8d31d064f1e28c6f63935e
Maksym Rybak <maksym@sarce.it>
2019-10-13 16:55:44 +0200

    remove unused files

---
+++ .docker/run-maksym-gitlab-container.ps1
@@ -0,0 +1,11 @@
+docker volume create gitlab-logs
+docker volume create gitlab-data 
+docker run --detach `
+	--name gitlab `
+	--restart always `
+	--hostname gitlab.example.com `
+	--publish 4443:443 --publish 4480:80 --publish 8222:22 `
+	--volume C:\dev\gitlab\config:/etc/gitlab `
+	--volume gitlab-logs:/var/log/gitlab `
+	--volume gitlab-data:/var/opt/gitlab `
+	maksymhub/gitlab-ce
